747e722b4fad42082b259e8c06c2e2e1edfa31b2
hungdao <m.hungdao64@gmail.com>
2024-12-08 11:13:40 +0700

    Update token storing

--- Spendings/Client/NetworkConfiguration.swift
+++ Spendings/Client/NetworkConfiguration.swift
@@ -2,14 +2,15 @@ import Foundation
 
 enum NetworkConfiguration {
     static let baseUrl: String = "https://firefly.lanhnguyen.dev/api"
-    static let token: String = NetworkConfiguration.environmentVariable(named: "api_token")
+    static let token: String = NetworkConfiguration.valueFromPlist(named: "token")
 
-    private static func environmentVariable(named: String) -> String {
-            let processInfo = ProcessInfo.processInfo
-            guard let value = processInfo.environment[named] else {
-                print("Missing Environment Variabled : '\(named)'")
-                return ""
-            }
-            return value
+    private static func valueFromPlist(named: String) -> String {
+        // Locate the plist file in the app bundle
+        if let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+           let dictionary = NSDictionary(contentsOfFile: path),
+           let token = dictionary[named] as? String {
+            return token
         }
+        return ""
+    }
 }
